bc9d55fc6ee286c06c4c312815b0fd68a39f955a
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2023-12-16 15:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 //your JS code here. If required.
-let input=parseInt(prompt())
+// let input=parseInt(prompt())
 
 function daysOfAYear(year) {
 
 	if(year%400===0){
-		return "366";
+		return 366;
 	}
 	else if(year%100===0){
-		return "365";
+		return 365;
 	}
 	else if(year%4===0){
-		return "366";
+		return 366;
 	}else {
-		return "365"
+		return 365
 	}
 
 	
 }
 
-alert(daysOfAYear(input))
+// alert(daysOfAYear(input))
 
 
 
